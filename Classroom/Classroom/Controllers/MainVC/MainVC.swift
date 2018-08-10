6346c9ed946c0b77c35a6b5f4fe0be6feb1c6626
ivan <zigzag69@ukr.net>
2018-08-10 10:21:17 +0300

    Rebuilt the application for xib

---
+++ Classroom/Classroom/Controllers/MainVC/MainVC.swift
@@ -0,0 +1,24 @@
+//
+//  MainVC.swift
+//  Classroom
+//
+//  Created by PAMacBook on 10.08.18.
+//  Copyright © 2018 Zigzag. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+//MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        preparations()
+    }
+    
+//MARK: - UI Preparations
+    func preparations() {
+        navigationController?.navigationBar.isHidden = true
+    }
+}
