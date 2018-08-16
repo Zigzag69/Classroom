52bbaf9ed62619cbf64dc43e37e61055d9f98d29
ivan <zigzag69@ukr.net>
2018-08-16 11:39:56 +0300

    Changed collectionView scroll logic

--- Classroom/Classroom/Controllers/MainVC/MainVC.swift
+++ Classroom/Classroom/Controllers/MainVC/MainVC.swift
@@ -12,6 +12,7 @@ class MainVC: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var gradient: UIImageView!
     @IBOutlet weak var classroomCV: UICollectionView!
+    @IBOutlet weak var classroomCVHeight: NSLayoutConstraint!
     
     let collectionViewCellID = "CollectionViewCell"
     
@@ -27,12 +28,17 @@ class MainVC: UIViewController {
     func preparations() {
         navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.barStyle = .blackOpaque
+        
         navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationBar.shadowImage = UIImage()
+        
         gradient.layer.shadowColor = UIColor.lightGray.cgColor
         gradient.layer.shadowRadius = 2
         gradient.layer.shadowOpacity = 0.5
         gradient.layer.shadowOffset = CGSize(width: 0, height: 4)
+        
+        let height = classroomCV.collectionViewLayout.collectionViewContentSize.height
+        classroomCVHeight.constant = height
     }
     
 //MARK: - Custom methods
