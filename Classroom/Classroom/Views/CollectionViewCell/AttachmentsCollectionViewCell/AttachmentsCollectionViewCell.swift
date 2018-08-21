7b1a338afa86edaef6af4a94c9f88af3eeeecf4a
ivan <zigzag69@ukr.net>
2018-08-21 10:35:19 +0300

    Added new collectionViewCells

---
+++ Classroom/Classroom/Views/CollectionViewCell/AttachmentsCollectionViewCell/AttachmentsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  AttachmentsCollectionViewCell.swift
+//  Classroom
+//
+//  Created by PAMacBook on 20.08.18.
+//  Copyright © 2018 Zigzag. All rights reserved.
+//
+
+import UIKit
+
+class AttachmentsCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
