da1184f181409d49288c2ffe8cf1981777cee806
ivan <zigzag69@ukr.net>
2018-08-22 15:49:26 +0300

    Changed textView height according to text

--- Classroom/Classroom/Controllers/MainVC/MainVC.swift
+++ Classroom/Classroom/Controllers/MainVC/MainVC.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MainVC: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var gradient: UIImageView!
     @IBOutlet weak var classroomCV: UICollectionView!
     @IBOutlet weak var classroomCVHeight: NSLayoutConstraint!
@@ -34,6 +35,10 @@ class MainVC: UIViewController {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = true
         navigationController?.navigationBar.barStyle = .blackOpaque
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 //MARK: - UI Preparations
@@ -64,3 +69,14 @@ class MainVC: UIViewController {
     }
 }
 
+extension MainVC: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        let fixedWidth = textView.frame.size.width
+        textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        let newSize = textView.sizeThatFits(CGSize(width: fixedWidth,
+                                                   height: CGFloat.greatestFiniteMagnitude))
+        var newFrame = textView.frame
+        newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+        textView.frame = newFrame
+    }
+}
